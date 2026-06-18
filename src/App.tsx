b845fc40621d5e4d7fd857f0ef4a791444f0c828
Sathvik Ram Lanka <109547847+RamLanka05@users.noreply.github.com>
2026-06-18 18:33:41 -0500

    Working toward a Dark/Light Mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import Navigation from './components/Navigation'
 import Hero from './components/Hero'
 import About from './components/About'
 import WorkShowcase from './components/WorkShowcase'
+import TopBar from './components/TopBar'
 
 function App() {
   return (
     <div className="bg-white dark:bg-gray-950 min-h-screen text-gray-900 dark:text-gray-200 font-sans antialiased">
       <div className="max-w-7xl mx-auto px-6 py-12 md:px-12 md:py-20 lg:px-24 flex flex-col md:flex-row md:justify-between gap-12">
-        
+        <TopBar />
         <header className="md:sticky md:top-0 md:h-screen md:w-1/2 flex flex-col justify-between py-12 md:py-24">
           <div>
             <Hero />
